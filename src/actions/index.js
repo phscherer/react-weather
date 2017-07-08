26fc54e3f2541638510e9641e18ae6fc480573b2
Paulo Henrique Scherer <paulo.h@systextil.com.br>
2017-07-08 13:04:31 -0300

    Creates ajax requests with axios

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '0a98393bb7cb267474d412dc3e228866';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
