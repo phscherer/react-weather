414c0932250e56e5fb50164677fa1daa549e648b
Paulo Henrique Scherer <paulo.h@systextil.com.br>
2017-07-29 20:53:59 -0300

    fix bugs

--- src/components/google_map.js
+++ src/components/google_map.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react';
 
 class GoogleMap extends Component {
   componentDidMount() {
-    new google.maps.Map(this.refs.maps, {
+    new google.maps.Map(this.refs.map, {
       zoom: 12,
       center: {
         lat: this.props.lat,
-        lon: this.props.lon
+        lng: this.props.lon
       }
     });
   }
